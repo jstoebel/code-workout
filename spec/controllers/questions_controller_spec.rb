4d616076657787ed36b55153d4a087cff8f9088c
Jacob Stoebel <stoebelj@berea.edu>
2016-03-18 10:39:52 -0400

    initial work on testing

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,19 +1,46 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
+
+  before(:each) do
+
+    FactoryGirl.create :confirmed_user, {:email => "test@test.com"}
+    FactoryGirl.create :question
+  end
+  # let(:valid_attributes) { { 
+  #   title: "question title",
+  #   body: "question body"
+  #  } }
+
 
   describe "GET index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "pulls all questions" do
+      get :index
+      assigns(:questions).should
+      expect(assigns(:teams)).to eq(Question.all)
+    end
+
+    it "renders the index view" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 
   describe "GET show" do
     it "returns http success" do
-      get :show
+      get :show {:id => 1}
       expect(response).to have_http_status(:success)
     end
+    
+    it "pulls the right question" do
+      get :show
+    end
+
   end
 
   describe "GET new" do
@@ -21,41 +48,50 @@ RSpec.describe QuestionsController, :type => :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
-  end
-
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    it "makes a new record" do
+      assigns(:question).should be_a_new(Question)
     end
   end
 
-  describe "GET edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "POST create" do
+  #   it "returns http success" do
+  #     question_params = FactoryGirl.attributes_for(:question)
+  #     expect {post(:create, 
+  #         {:question => question_params
+  #         }
+  #       ) 
+  #     }.to change(Question, :count).by(1)
+  #     expect(response).to have_http_status(:success)
 
-  describe "GET update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
+  #   end
+  # end
 
-  describe "GET delete" do
-    it "returns http success" do
-      get :delete
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET update" do
+  #   it "returns http success" do
+  #     get :update
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET delete" do
+  #   it "returns http success" do
+  #     get :delete
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET destroy" do
+  #   it "returns http success" do
+  #     get :destroy
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
