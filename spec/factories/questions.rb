7bbe4c27c1ac07e25980b8918b284e37aab2377c
Jacob Stoebel <stoebelj@berea.edu>
2016-03-18 10:39:52 -0400

    initial work on testing

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :question do
-  end
-end
+    FactoryGirl.define do
+
+      factory :question do
+        title "a question"
+        body "question body text"
+        tags "for loops; conditionals"
+        user
+        exercise
+
+      end
+    end
