6140f70de012e0247104567afd6bbf1423ba36da
vntmb <tembov@berea.edu>
2016-04-20 11:33:59 -0400

    Schema manual merge after factory fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411161514) do
+ActiveRecord::Schema.define(version: 20160409163140) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -339,11 +339,8 @@ ActiveRecord::Schema.define(version: 20160411161514) do
     t.string   "tags"
     t.integer  "user_id"
     t.integer  "exercise_id"
-    t.integer  "upvote"
-    t.integer  "downvote"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "down_vote"
     t.string   "flags"
   end
 
